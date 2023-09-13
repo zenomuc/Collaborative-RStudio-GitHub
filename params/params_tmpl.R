e71990fc7434ae45387cc5fc4073c73ad89a52d6
Zeno <maiagtx@gmail.com>
2023-09-13 16:10:25 +0200

    add variables

--- params/params_tmpl.R
+++ params/params_tmpl.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 4
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "micobacterium_floccculosum"
     
 # e.g. "red"    
-color <- 
+color <- "blue2"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
